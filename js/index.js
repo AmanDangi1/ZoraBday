486f9fa37e728d4c3c5a44dc9a0000d2aef3fb60
AmanDangi1 <78592856+AmanDangi1@users.noreply.github.com>
2022-04-28 02:38:20 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -296,7 +296,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/7OYpKwaYsrw");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/d6FyOeiRh0c");
   ifrm.style.width = `${w}px`;
   ifrm.style.height = `${h}px`;
   ifrm.style.border = '1px';
